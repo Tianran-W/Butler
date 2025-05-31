bda2e0619e93f48ec2e0429345ae569969f9f127
qingfangYu <yuqingfang2004@outlook.com>
2025-06-01 00:58:48 +0800

    Cyptorcs:紧急重构，LifeSpanMaterial

--- src/main/java/com/example/ERPApplication.java
+++ src/main/java/com/example/ERPApplication.java
@@ -5,13 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.LendAndReturn.Lend.mapper")
-@MapperScan("com.example.LendAndReturn.Return.mapper")
-@MapperScan("com.example.LendAndReturn.LifeSpanTracker.mapper")
-@MapperScan("com.example.inventory.approval.mapper")
-@MapperScan("com.example.inventory.category.mapper")
-@MapperScan("com.example.inventory.material.mapper")
-
+@MapperScan("com.example.mapper")
 public class ERPApplication {
     public static void main(String[] args) {
         SpringApplication.run(ERPApplication.class, args);
--- src/main/java/com/example/inventory/common/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.inventory.common.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
